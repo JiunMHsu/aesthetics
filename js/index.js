59ceae83cf12d23ff8cbffddf98f8665c1ca822c
Jiun Ming Hsu <hsujm219@gmail.com>
2022-08-30 05:57:07 -0300

    homepage animation made

--- js/index.js
+++ js/index.js
@@ -2,8 +2,25 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // photo switch animation
-    
+    let photoId = 1;
+
+    let photoShown = document.getElementById(photoId);
+    photoShown.style.display = "flex";
+
+    function changePhoto() {
+        photoShown.style.display = "none";
+        photoId++;
+
+        photoShown = document.getElementById(photoId);
+        photoShown.style.display = "flex";
+
+        photoId = (photoId == 9) ? 0 : photoId;
+    }
+
+    setInterval(() => {
+        // change displayed photo
+        // by setting display attribute
+        changePhoto();
+    }, 3000)
 }) 
-
-
 
